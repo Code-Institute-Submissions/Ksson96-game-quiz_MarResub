30b8d1a227701c26fec070edd844e90b1139e34d
Anton Andersson <anton.andersson001@live.se>
2021-11-24 14:45:26 +0000

    Add eventlistener and class toggle to start button

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+const gameStart =  document.getElementById('start')
+gameStart.addEventListener('click', runGame);
+
+function runGame() {
+    let startHeading = document.getElementById('start-heading');
+    gameStart.classList.add('hide');
+    startHeading.classList.add('hide'); 
+
+}
+
+function checkAnswer () {
+
+}
+
+function calculateScore() {
+
+}
+
+function displayQuestion() {
+
+}
