61e868dd645c31256ad9d55fdc6ae34637129cec
Anton Andersson <anton.andersson001@live.se>
2021-11-24 14:55:30 +0000

    Remove .hide class on game container in function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,10 @@ gameStart.addEventListener('click', runGame);
 
 function runGame() {
     let startHeading = document.getElementById('start-heading');
+    let gameContainer = document.getElementById('game-container');
     gameStart.classList.add('hide');
     startHeading.classList.add('hide'); 
+    gameContainer.classList.remove('hide');
 
 }
 
